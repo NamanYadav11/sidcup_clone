bd4b8290ef8a2fecd60dc8d9a8e8869b36a340cc
NamanYadav <devwithnaman@gmail.com>
2023-08-05 15:07:08 +0530

    complete

--- README.md
+++
@@ -1,1 +0,0 @@
-# sidcup_clone
--- index.js
+++ index.js
@@ -8,6 +8,19 @@ document.addEventListener("mousemove",function(dets){
     blur.style.top=dets.y-190+"px"
 })
 
+var h4all=document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        crsr.style.scale = 3;
+        crsr.style.border = "1px solid #fff";
+        crsr.style.backgroundColor = "transparent";
+    })
+    elem.addEventListener("mouseleave",function(){
+        crsr.style.scale = 1;
+        crsr.style.border = "0px solid #95c11E";
+        crsr.style.backgroundColor = "#95c11E";
+    })
+})
 
 
 
@@ -37,3 +50,79 @@ gsap.to("#main",{
         scrub:2
     }
 })
+
+
+gsap.from("#about-us,about-us-in",{
+    y:50,
+    opacity:0,
+    stragger:0.6,
+    duration:1.5,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers: true,
+        start:"top 75%",
+        end:"top 60%",
+        scrub:3
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    stragger:0.6,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers: true,
+        start:"top 75%",
+        end:"top 60%",
+        scrub:3
+    }
+})
+
+gsap.from("#tag-line",{
+    y:70,
+    stragger:0.6,
+    duration:1.5,
+    scrollTrigger:{
+        trigger:"#col1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:3
+    }
+
+})
+
+
+
+gsap.from("#col1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#col1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#col2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#col1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+
+
